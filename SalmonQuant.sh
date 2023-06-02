6a4a7b9952b5fbc1a61ebecc4861b6803b5538e9
Jonathan Anzules <jonanzule@gmail.com>
2023-06-01 19:46:35 -0700

    Began the salmon quantification script
    I need to update the salmon package to the most current version

--- SalmonQuant.sh
+++ SalmonQuant.sh
@@ -1,1 +1,26 @@
 #!/bin/bash
+
+############################################
+#
+# Aligning and quantifying with salmon
+# 
+
+
+cd ../Data
+
+# mkdir SalmonQuant
+
+for file in trimmed_fastq/*.fastq.gz
+    do 
+    
+    basename=$(basename "${file}" _trimmed.fastq.gz)
+
+    echo "$basename"
+    # Run Salmon to align and quantify the reads
+    # salmon quant \
+    #     -i hg19/hg19_salmon/ \
+    #     -l A \
+    #     -r "${file}" \
+    #     -p 25 \
+    #     -o SalmonQuant
+done
