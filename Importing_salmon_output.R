8b4d57c684e6299d9ae00750908cf88e696e45f9
Jonathan Anzules <jonanzule@gmail.com>
2023-06-08 22:43:50 -0700

    Created a deseq object

--- Importing_salmon_output.R
+++ Importing_salmon_output.R
@@ -1,19 +1,17 @@
-install.packages("GenomicFeatures")
+# install.packages("BiocManager") # needed to install Genomic Features
+# BiocManager::install("GenomicFeatures") #Used for importing and makign the tx2gene mapping
+# BiocManager::install("tximport") #needed to prepare the data for deseq
+# BiocManager::install("DESeq2") #for differential gene expression analysis
 
-install.packages("BiocManager")
-install.packages("Rtools")
 
-BiocManager::install("GenomicFeatures")
 
-install.Rtools(check = TRUE, check_r_update = TRUE, GUI = TRUE)
-install("installr")
-
+#######################################
+# Generating the tx2gene 
 library(GenomicFeatures)
-
 #You have to set the working dyrectory to where the code is at
 
 txdb <- makeTxDbFromGFF("../Data/hg19/gencode.v19.chr_patch_hapl_scaff.annotation.gtf")
 keytypes(txdb)
 k <- keys(txdb, keytype = "TXNAME")
 tx2gene <- select(txdb, k, "GENEID", "TXNAME")
-tx2gene
+
