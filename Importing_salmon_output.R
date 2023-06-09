0441eb3356258ea8161c872c2cb32743733778cd
Jonathan Anzules <jonanzule@gmail.com>
2023-06-08 17:35:55 -0700

    Figured out how to make a tx2gene file for tximport

--- Importing_salmon_output.R
+++ Importing_salmon_output.R
@@ -7,3 +7,13 @@ BiocManager::install("GenomicFeatures")
 
 install.Rtools(check = TRUE, check_r_update = TRUE, GUI = TRUE)
 install("installr")
+
+library(GenomicFeatures)
+
+#You have to set the working dyrectory to where the code is at
+
+txdb <- makeTxDbFromGFF("../Data/hg19/gencode.v19.chr_patch_hapl_scaff.annotation.gtf")
+keytypes(txdb)
+k <- keys(txdb, keytype = "TXNAME")
+tx2gene <- select(txdb, k, "GENEID", "TXNAME")
+tx2gene
